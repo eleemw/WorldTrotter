af549c7d7cf31cff1a959fa34fdc275ba5a326dd
eleemw <eleemw+codenews@gmail.com>
2020-12-23 20:39:05 -0800

    Initial Commit

---
+++ WorldTrotter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Erica Murdock-Waters on 12/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
